7930536f799b4d474135601e32a2baea8a400974
Mishail <misha.123qq@ya.ru>
2025-08-17 22:43:18 +0300

    изменил заполнение бд

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -16,21 +16,3 @@ VALUES
 (4, 'Триллер'),
 (5, 'Документальный'),
 (6, 'Боевик');
-
-MERGE INTO film
-KEY (id)
-VALUES
-  ('1', 'mona', 'trash', '2023-01-01', 120, 1),
-  ('2', 'витя', 'чёрт ебаный', '2023-01-02', 130, 2),
-  ('3', 'mona3', 'trash3', '2023-01-03', 140, 1);
-MERGE INTO users
-KEY (id)
-VALUES
-  ('1', 'Иван Иванов', 'ivan@mail.ru', 'ivan', '1990-01-01'),
-  ('2', 'Петр Петров', 'petr@mail.ru', 'petr', '1985-02-02');
-MERGE INTO film_genre
-KEY (film_id, genre_id)
-VALUES
-  (1, 1),
-  (1, 2),
-  (2, 1);
--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -1,35 +1,17 @@
 package ru.yandex.practicum.filmorate;
 
 import lombok.RequiredArgsConstructor;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
 import org.springframework.context.annotation.Import;
-import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.UserDbStorage;
-import ru.yandex.practicum.filmorate.storage.UserStorage;
 
-import java.util.Optional;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @JdbcTest
 @AutoConfigureTestDatabase
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 @Import({UserDbStorage.class})
 class FilmorateApplicationTests {
-    @Autowired
-    private UserStorage userStorage;
 
-    @Test
-    public void testFindUserById() {
-        Optional<User> userOptional = Optional.ofNullable(userStorage.findById(1).orElse(null));
-
-        assertThat(userOptional)
-                .isPresent()
-                .hasValueSatisfying(user ->
-                        assertThat(user).hasFieldOrPropertyWithValue("id", 1)
-                );
-    }
 }
